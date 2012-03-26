ee90befc606f04467ab95aa60c78ee752e0c2b73
frederic.n.leroux@gmail.com <frederic.n.leroux@gmail.com>
2012-03-26 23:35:59 +0200

    ecriture de stub : ok

--- src/pod-api/etape2/StubGenerator.java
+++ src/pod-api/etape2/StubGenerator.java
@@ -9,20 +9,20 @@ public class StubGenerator {
 	public static File introspection(Object o) throws IOException{
 
 		String className=o.getClass().getSimpleName();
-		File f = new File(className + "_stub.java");
+		File f = new File("./stub/"+className + "_stub.java");
 		BufferedWriter buffer = new BufferedWriter(new FileWriter(f));
         String text = "";
-		text = "public class " + className + "_stub " + "extends SharedObject, ";
-		text = text + "implements "   + className + "_itf, " + "java.io.Serializable {";
-		text = text + '\n';
+		text += "public class " + className + "_stub " + "extends SharedObject, ";
+		text += "implements "   + className + "_itf, " + "java.io.Serializable {";
+		text += "\n" + "\n";
 
         // Constructor
-		text = text + '\t'+ "public " + className + "_stub (int id, Object o){";
-		text = text + '\n';
-		text = text + '\t'+'\t'+"super(id,o);";
-		text = text + '\n';
-        text = text + '\t'+"}";
-        text = text + '\n';
+		text += "\t"+ "public " + className + "_stub (int id, Object o){ ";
+		text += "\n";
+		text += "\t"+"\t"+"super(id,o);";
+		text += "\n";
+        text += "\t"+"}";
+        text += "\n";
 
         // Methods
 		Method[] methods = o.getClass().getMethods();
@@ -31,70 +31,81 @@ public class StubGenerator {
 			String method_name = m.getName();
 		    int modifier = m.getModifiers();
             String return_type = m.getReturnType().getSimpleName();
-            Type[] parameter = m.getParamaterType();
-            Type[] exception = m.getExceptionTypes();
-            
+            Class[] parameter = m.getParameterTypes();
+            Class[] exception = m.getExceptionTypes();
+            text += "\n";
             // Access modifier
             if(Modifier.isPublic(modifier)){
-                text = text + '\t'+"public ";
+                text += "\t"+"public ";
             }
             if(Modifier.isPrivate(modifier)){
-               text = text + '\t'+"private ";
+                text += "\t"+"private ";
             }
 		    if(Modifier.isProtected(modifier)){
-                text =text + '\t'+"protected ";
+                text += "\t"+"protected ";
             }
             
             // return type
-            text = text + return_type + " " ;
+            text += return_type + " " ;
 
             // name
-            text = text + method_name +"(";
+            text += method_name +"(";
             
             // parameters
             int arg = 0;
+            String arg_line = "";
             String parameter_line = "";
-            for(Type t : parameter){
-                parameter_line += t.getSimpleName + " arg"+arg+", ";
+            for(Class t : parameter){
+                parameter_line += t.getSimpleName() + " arg"+arg+", ";
+                arg_line += " arg"+arg+", ";
+                arg+=1;
             }
             if(!parameter_line.equals("")){
-                parameter_line = parameter_line.subString(0,parameter_line.length()-3);
+                parameter_line = parameter_line.substring(0,parameter_line.length()-2);
+                arg_line = arg_line.substring(0,arg_line.length()-2);
             }
-            text = text + parameter_line + ") ";
+            
+            text += parameter_line + ") ";
             
             // exception
             String exception_line = "";
-            for(Type e : exception){
-                exception_line += e.getSimpleName +", ";
+            for(Class e : exception){
+                exception_line += e.getSimpleName()+", ";
             }
             if(!exception_line.equals("")){
-                exception_line = "throws "+exception_line.subString(0,exception_line.length() - 3);
+                exception_line = "throws "+exception_line.substring(0,exception_line.length() - 2);
             }
-            text = text + exception_line;
-            text = text + "{";
-            text = text = '\n';
+            text += exception_line;
+            text += "{";
+            text += "\n";
 
             // body of method
                 
-            text = text + '\t' + '\t';
-            text = className + "object = ("+className+").obj ;";
-            text = text + '\n' + '\t' +'\t';
+            text += "\t" + "\t";
+            text += "("+className + ")object = ("+className+").obj ;";
+            text += "\n" + "\t" +"\t";
             if(return_type.equals("void")){
-                text = text + "object."+method_name+parameter_line+";";
+                text += "object."+method_name+"("+arg_line+");";
             }else{
-                text = text + "return object."+method_name+paramater_line+";";
+                text += "return object."+method_name+"("+arg_line+");";
             }
-            text = text + '\n' + '\t' + "}";
+            text += "\n" + "\t" + "}";
+            text += "\n";
         }
-        text = text + '\n' + '}';
+        text += "\n" + '}';
         buffer.write(text);
-        witer.close();
+        buffer.close();
         return f;
 	}
 
     public static void main(String args[]){
-        Compteur c = new Compteur();
-        this.introspection(c);
+        Sentence s = new Sentence();
+        try{
+            StubGenerator.introspection(s);
+        }catch(IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
         System.exit(0);
     }
 }	
